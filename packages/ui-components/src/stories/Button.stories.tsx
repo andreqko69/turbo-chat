d2b2cefde55c9c7f1395c73bb528fdf08bf7094f
andreqko <andreqkodev@gmail.com>
2025-05-05 22:12:27 +0300

    Refactor: Organize components into atomic structure

--- packages/ui-components/src/stories/Button.stories.tsx
+++ packages/ui-components/src/stories/Button.stories.tsx
@@ -1,11 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
+import { User } from 'lucide-react';
 
-import { Button } from '~/components/ui/button';
+import { Button } from '~/components/atoms/button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'UI/Button',
+  title: 'Atoms/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -14,9 +15,20 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   backgroundColor: { control: 'color' },
-  // },
+  argTypes: {
+    size: { control: 'select', options: ['default', 'sm', 'lg', 'icon'] },
+    variant: {
+      control: 'select',
+      options: [
+        'default',
+        'secondary',
+        'destructive',
+        'ghost',
+        'outline',
+        'link',
+      ],
+    },
+  },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
@@ -60,9 +72,25 @@ export const Outline: Story = {
   },
 };
 
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    children: 'Link',
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    variant: 'default',
+    size: 'icon',
+    children: <User />,
+  },
+};
+
 export const Large: Story = {
   args: {
     size: 'lg',
+    variant: 'default',
     children: 'Button',
   },
 };
@@ -70,6 +98,7 @@ export const Large: Story = {
 export const Small: Story = {
   args: {
     size: 'sm',
+    variant: 'default',
     children: 'Button',
   },
 };
